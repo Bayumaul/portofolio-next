476ecf4191042dc69d7d11b0c4a1f459c70760a8
Bayumaul <bayu.maulanaikhsan123@gmail.com>
2023-08-10 10:44:43 +0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500">
+    <footer className="mb-10 px-4 text-center text-gray-500 dark:text-white">
       <small className="mb-2 block text-xs">
         &copy; {currentYear} Bayu Maulana Ikhsan. All rights reserved.
       </small>
