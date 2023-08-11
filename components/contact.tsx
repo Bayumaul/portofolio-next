9833bb7a2d6b2276d2c89c9925d15a7016fcd95a
Bayumaul <bayu.maulanaikhsan123@gmail.com>
2023-08-11 08:49:32 +0700

    fixed background

--- components/contact.tsx
+++ components/contact.tsx
@@ -29,6 +29,8 @@ export default function Contact() {
         once: true,
       }}
     >
+      <div className="bg-[#fbe2e3] absolute bottom-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+
       <SectionHeading>Contact me</SectionHeading>
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
@@ -58,7 +60,7 @@ export default function Contact() {
           placeholder="Your email"
         />
         <textarea
-          className="h-52 bg-white my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          className="h-40 bg-white my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="message"
           placeholder="Your message"
           required
