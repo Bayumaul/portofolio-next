524a3f331ac6bbc48aecc7f8cf698f4d84a88916
Bayumaul <bayu.maulanaikhsan123@gmail.com>
2023-11-01 14:07:50 +0700

    update navbar

--- lib/data.ts
+++ lib/data.ts
@@ -24,10 +24,10 @@ export const links = [
     name: "Skills",
     hash: "#skills",
   },
-  {
-    name: "Experience",
-    hash: "#experience",
-  },
+  // {
+  //   name: "Experience",
+  //   hash: "#experience",
+  // },
   {
     name: "Contact",
     hash: "#contact",
